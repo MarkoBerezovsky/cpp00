bec745115ccb4fe6b669e116d1d0546ce25f5079
martingrossman <martingrossman@users.noreply.github.com>
2016-12-14 16:38:16 +0100

    Add files via upload

---
+++ 01_Output/eclipse/shapes.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : shapes.cpp
+// Author      : mg
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "square_empty.h"
+using namespace std;
+
+int main() {
+//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	square_empty(5,5);
+	return 0;
+}
+
